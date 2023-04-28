3bc84f6f97eedfa129d35d738d7d45f2e406520d
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-04-28 21:26:07 +0530

    Time: 21 ms (74.19%), Space: 11.5 MB (18.19%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> output;
+    void traverse(Node* root) {
+        // Base case: if the tree is empty...
+        if(root == NULL) return;
+        // Push the value of the root node to the output...
+        output.push_back(root->val);
+        // Recursively traverse each node in the children array...
+        for(auto node:root->children)
+            traverse(node);
+    }
+    vector<int> preorder(Node* root) {
+        output.clear();
+        traverse(root);
+        return output;
+    }
+};
