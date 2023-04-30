de4ead039effa4c18e642b7115cf6ab4a49a9d9f
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-04-30 16:47:12 +0530

    Time: 7 ms (93.16%), Space: 10.6 MB (95.09%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,36 +1,30 @@
-// class Solution {
-// public:
-//     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-//         int m = mat.size() , n= mat[0].size();
-//         vector<vector<int>>v(r,vector<int>(c));
-//         queue<int>q;
-//         if(m*n == r*c){
-//             for(int i=0;i<m;i++){
-//                 for(int j=0;j<n;j++){
-//                     q.push(mat[i][j]);
-//                 }
-//             }
-//             for(int i=0;i<r;i++){
-//                 for(int j=0;j<c;j++){
-//                     v[i][j] = q.front();
-//                     q.pop();
-//                 }
-//             }
-//             return v;
-//         }
-//         return mat;
-//     }
-// };
-// 
-
 class Solution {
 public:
-    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
-        int n = nums.size(), m = nums[0].size();
-        if (n * m != r * c) return nums;
-        vector<vector<int>> ans(r, vector<int>(c));
-        for (int i = 0; i < r * c; i++) 
-        ans[i/c][i%c] = nums[i/m][i%m];
-        return ans;
-    }
+    vector<vector<int>> matrixReshape(vector<vector<int>>& 
+        mat, int r, int c);
 };
+/**************************************************************/
+vector<vector<int>> Solution::matrixReshape 
+        (vector<vector<int>>& mat, int r, int c) {
+    if (mat[0].size()*mat.size() != r*c) {
+        return mat;
+    }
+    vector<vector<int>> v;
+    vector<int> aux(c, 0);
+    int i, j, rows = mat.size(), columns = mat[0].size(), row=0, column=0;
+    for (i = 0; i < r; ++i) {
+        v.push_back(aux);
+    }
+    for (i = 0; i < rows; ++i) {
+        for (j = 0; j < columns; ++j) {
+            v[row][column] = mat[i][j];
+            /* indexes update */
+            ++column;
+            if (column == c) {
+                column = 0;
+                ++row;
+            }
+        }
+    }
+    return v;
+}
