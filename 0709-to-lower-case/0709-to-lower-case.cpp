17b7dd9357e112a4dbbfaa1890790a430b94ccd8
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-02 18:23:04 +0530

    Time: 4 ms (40.25%), Space: 6.3 MB (12.54%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+      for(int i =0;i<s.size();i++){
+          if(s[i]>='A'&& s[i]<='Z')
+              s[i]=s[i]-'A'+'a';
+      }
+      return s;
+       
+        
+    }
+};
