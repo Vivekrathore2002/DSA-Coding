b849756cbcd1bc1fcbd03d2d6197c69ed23ff3a0
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-04 12:08:40 +0530

    Time: 5 ms (10.97%), Space: 7 MB (79.51%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,47 +1,43 @@
 class MyQueue {
 public:
-    stack<int> s1;
-    stack<int> s2;
-    /** Initialize your data structure here. */
+    stack<int>s;
     MyQueue() {
-        
     }
     
-    /** Push element x to the back of queue. */
     void push(int x) {
-        while(!s1.empty()) {
-            s2.push(s1.top());
+        s.push(x);
+    }
+    
+    int pop() {
+        stack<int>s1;
+        while(!s.empty()){
+            s1.push(s.top());
+            s.pop();
+        }
+        int tp=s1.top();
+        s1.pop();
+        while(!s1.empty()){
+            s.push(s1.top());
             s1.pop();
         }
-        s2.push(x);
-        while(!s2.empty()) {
-            s1.push(s2.top());
-            s2.pop();
-        }
+        return tp;
     }
     
-    /** Removes the element from in front of queue and returns that element. */
-    int pop() {
-        int curr = s1.top();
-        s1.pop();
-        return curr;
-    }
-    
-    /** Get the front element. */
     int peek() {
-        return s1.top();
+        stack<int>s1;
+        while(!s.empty()){
+            s1.push(s.top());
+            s.pop();
+        }
+        int tp=s1.top();
+        while(!s1.empty()){
+            s.push(s1.top());
+            s1.pop();
+        }
+        return tp;
     }
     
-    /** Returns whether the queue is empty. */
     bool empty() {
-        return s1.empty();
+        return s.empty();
     }
 };
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
