524fb5a200120206b9635fafaff30ecc7fd68239
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-10 18:45:57 +0530

    Time: 39 ms (72.52%), Space: 19 MB (85.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,22 +1,53 @@
+// //Striver Solution
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         vector<vector<int>> mergedIntervals;
+//         if(intervals.size() == 0) {
+//             return mergedIntervals; 
+//         }
+//         sort(intervals.begin(), intervals.end());
+//         vector<int> tempInterval = intervals[0]; 
+        
+//         for(auto it : intervals) {
+//             if(it[0] <= tempInterval[1]) {
+//                 tempInterval[1] = max(it[1], tempInterval[1]); 
+//             } else {
+//                 mergedIntervals.push_back(tempInterval); 
+//                 tempInterval = it; 
+//             }
+//         }
+//         mergedIntervals.push_back(tempInterval);
+//         return mergedIntervals; 
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergedIntervals;
-        if(intervals.size() == 0) {
-            return mergedIntervals; 
-        }
-        sort(intervals.begin(), intervals.end());
-        vector<int> tempInterval = intervals[0]; 
-        
-        for(auto it : intervals) {
-            if(it[0] <= tempInterval[1]) {
-                tempInterval[1] = max(it[1], tempInterval[1]); 
-            } else {
-                mergedIntervals.push_back(tempInterval); 
-                tempInterval = it; 
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        int n=intervals.size();
+        for(int i=0;i<n;i++){
+            if(ans.empty()){
+                ans.push_back(intervals[i]); //[1,4]       [3,6] == [1,6]
+            }
+            else{
+                vector<int>&v=ans.back();
+                int y=v[1];
+                //Overlapping
+                if(intervals[i][0]<=y){
+                    v[1]=max(y,intervals[i][1]);
+                }
+                //notOverlapping
+                else{
+                    ans.push_back(intervals[i]);
+                }
             }
         }
-        mergedIntervals.push_back(tempInterval);
-        return mergedIntervals; 
+        return ans;
+
     }
 };
