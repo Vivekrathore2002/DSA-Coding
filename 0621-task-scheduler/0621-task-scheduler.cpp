03975ec157eccf48f454645b731e494768823c67
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-13 00:10:31 +0530

    Time: 72 ms (70.02%), Space: 34.3 MB (91.50%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int N=tasks.size();
+        unordered_map<char,int> mp;
+        int count=0;
+        for(auto e:tasks){
+            mp[e]++;
+            count=max(count,mp[e]);
+        }
+        int ans=(count-1)*(n+1);
+        for(auto e:mp){
+            if(e.second==count){
+                ans++;
+            }
+        }
+        return max(N,ans);
+    }
+};
