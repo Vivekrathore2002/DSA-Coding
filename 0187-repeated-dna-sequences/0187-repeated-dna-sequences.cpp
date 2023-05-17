1b587174f32da51c012cd188893ef90bf80f01a9
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-17 23:41:40 +0530

    Time: 59 ms (77.71%), Space: 23.4 MB (45.89%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int> mp;
+        int n=s.size();
+        for(int i=0;i<=n-10;i++){
+            string str=s.substr(i,10);
+            mp[str]++;
+        }
+        vector<string> ans;
+        for(auto &x:mp){
+            if(x.second>1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
