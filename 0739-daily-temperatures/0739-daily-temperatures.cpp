8497efb0e91def9f67240ebc2554858e8666dccb
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-21 14:33:21 +0530

    Time: 197 ms (85.23%), Space: 98.6 MB (78.34%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,21 +6,51 @@
 // 1st will be just start from the end of the tempreture and check towards the left side whether the small element have big element at right side or not
 // this method will take extra space(o(n))
 
+// 1st method by stack
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         1st method(stack):
+//         stack<int> s;
+//         vector<int> ans(temperatures.size(), 0);
+//         for (int i = 0; i < temperatures.size(); i++) {
+//             while (!s.empty() && temperatures[s.top()] < temperatures[i]) {
+//                 int prevIndex = s.top();
+//                 s.pop();
+//                 ans[prevIndex] = i - prevIndex;
+//             }
+//             s.push(i);
+//         }
+//         return ans;
+//     }
+// };
+
+
+// 2nd method without stack
+#include <vector>
 
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> s;
-        vector<int> ans(temperatures.size(), 0);
-        for (int i = 0; i < temperatures.size(); i++) {
-            while (!s.empty() && temperatures[s.top()] < temperatures[i]) {
-                int prevIndex = s.top();
-                s.pop();
-                ans[prevIndex] = i - prevIndex;
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (ans[j] > 0) {
+                    j = j + ans[j];
+                } else {
+                    j = n;  // Exit loop if ans[j] is 0
+                }
             }
-            s.push(i);
+            if (j < n) {
+                ans[i] = j - i;
+            }
         }
         return ans;
     }
 };
+
+
 
