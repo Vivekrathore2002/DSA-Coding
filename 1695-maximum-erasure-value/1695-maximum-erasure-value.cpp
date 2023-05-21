55b67cd9682386b6632a0214ab7abb15367e5492
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-21 12:40:38 +0530

    Time: 690 ms (6.44%), Space: 156.7 MB (7.38%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,4 +1,15 @@
 // https://www.youtube.com/watch?v=WG6Kok6tIyg&ab_channel=CodewithAlisha
+
+// for example there was an array [4,2,4,5,6]
+// first start and end will start from  0 index means 4
+//     they will check whether the element is present in the set or not;
+//     if not than direct insert(not go to 2nd while loop)
+//     else the loop will be executed since the element was found before coming to the end
+//     if loop executed once than we go to 1st loop further,
+//     for example 4 will repeat than simply loop will minus the 4 from sum 
+//     and new 4 will be added to the sum after end of 2nd while loop
+// if by chance the sum gets lower from the last iteration such as given in the video than the max value will be taken to return
+    
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
@@ -20,3 +31,8 @@ public:
         return ans;
     }
 };
+
+
+
+
+    
