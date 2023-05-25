ec48896f46ba3a1ea576d40872b4ea31c2d0c42d
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-25 23:44:39 +0530

    Time: 40 ms (69.51%), Space: 19 MB (63.10%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,53 +1,25 @@
-// //Striver Solution
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//         vector<vector<int>> mergedIntervals;
-//         if(intervals.size() == 0) {
-//             return mergedIntervals; 
-//         }
-//         sort(intervals.begin(), intervals.end());
-//         vector<int> tempInterval = intervals[0]; 
-        
-//         for(auto it : intervals) {
-//             if(it[0] <= tempInterval[1]) {
-//                 tempInterval[1] = max(it[1], tempInterval[1]); 
-//             } else {
-//                 mergedIntervals.push_back(tempInterval); 
-//                 tempInterval = it; 
-//             }
-//         }
-//         mergedIntervals.push_back(tempInterval);
-//         return mergedIntervals; 
-//     }
-// };
-
-
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        int n=intervals.size();
-        for(int i=0;i<n;i++){
-            if(ans.empty()){
-                ans.push_back(intervals[i]); //[1,4]       [3,6] == [1,6]
-            }
-            else{
-                vector<int>&v=ans.back();
-                int y=v[1];
-                //Overlapping
-                if(intervals[i][0]<=y){
-                    v[1]=max(y,intervals[i][1]);
-                }
-                //notOverlapping
-                else{
-                    ans.push_back(intervals[i]);
-                }
-            }
-        }
-        return ans;
+        vector<vector<int>> result;
 
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
+            return a[0] < b[0]; 
+        });
+        
+        int i;
+        for (i = 0; i < intervals.size() - 1; i++) {
+            // no overlap
+            if (intervals[i][1] < intervals[i+1][0]) {
+                result.push_back(intervals[i]);
+                continue;
+            }
+            // overlap
+            intervals[i+1][0] = intervals[i][0];
+            intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1]);
+        }
+        result.push_back(intervals[i]);
+
+        return result;
     }
 };
