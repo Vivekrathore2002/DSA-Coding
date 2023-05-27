00de07c50814d40aa1e766a9f95b4e8e04ad9dc4
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-27 23:53:19 +0530

    Time: 410 ms (12.78%), Space: 14 MB (99.07%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<bool> vis(n, 0);
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                count++;
+                dfsPath(i, vis, stones, n);
+            }
+        }
+
+        return n - count;
+    }
+
+    void dfsPath(int idx, vector<bool> &vis, vector<vector<int>> &stones, int &n){
+        vis[idx] = 1;
+
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                // check if the cur stonr is at same row or col with parent stone
+                if(stones[i][0] == stones[idx][0] || stones[i][1] == stones[idx][1]){
+                    dfsPath(i, vis, stones, n);
+                }
+            }
+        }
+    }
+};
