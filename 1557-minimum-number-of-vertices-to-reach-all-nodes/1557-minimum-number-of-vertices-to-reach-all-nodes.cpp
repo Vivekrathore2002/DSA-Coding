21570e8f8760546e95b92f4244140aa1ea375337
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-05-27 23:44:06 +0530

    Time: 303 ms (77.84%), Space: 94.5 MB (58.35%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<bool> visited(n);
-        for(auto edge:edges){
-            visited[edge[1]]=true;
-        }
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+         vector<int> res, visited(n);
+        // mark the node which can be visited by e 
+        for (auto& e: edges)
+            visited[e[1]] = 1;
+        // push the node which are not visited because we will need those nodes to traverse the whole graph
+        for (int i = 0; i < n; ++i)
+            if (visited[i] == 0)
+                res.push_back(i);
+        return res;
     }
 };
