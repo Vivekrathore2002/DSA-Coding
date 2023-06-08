2d20c50e84ea50490e113138eb9c179aff0ba930
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-06-08 14:31:57 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.cpp
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -30,27 +30,52 @@ using namespace std;
 
 
 // Approach 2 reccursion+memoization
+// class Solution{
+// 	public:
+// 	int solve(int n,vector<int> &dp){
+// 	    if(n==0) return 0;
+// 	    if(dp[n]!=-1){
+// 	        return dp[n];
+// 	    }
+// 	    int ans=n;
+// 	    for(int i=1;i*i<=n;i++){
+// 	        int temp = i*i;
+// 	        ans=min(ans,1+solve(n-temp,dp));
+// 	    }
+// 	    dp[n]=ans;
+// 	    return dp[n];
+// 	}
+// 	int MinSquares(int n)
+// 	{
+// 	    vector<int> dp(n+1,-1);
+// 	    return solve(n,dp);
+// 	}
+// };
+
+// approach - 3 tabulation or bottom up approach
 class Solution{
 	public:
-	int solve(int n,vector<int> &dp){
-	    if(n==0) return 0;
-	    if(dp[n]!=-1){
-	        return dp[n];
+	int solve(int n){
+	    vector<int> dp(n+1,INT_MAX);
+	    dp[0]=0;
+	    for(int i=1;i<=n;i++){
+	        
+	        for(int j=1;j*j<=n;j++){
+	            int temp = j*j;
+	            if(i-temp>=0)
+	            dp[i]=min(dp[i],1+dp[i-temp]);
+	        }
 	    }
-	    int ans=n;
-	    for(int i=1;i*i<=n;i++){
-	        int temp = i*i;
-	        ans=min(ans,1+solve(n-temp,dp));
-	    }
-	    dp[n]=ans;
 	    return dp[n];
 	}
 	int MinSquares(int n)
 	{
-	    vector<int> dp(n+1,-1);
-	    return solve(n,dp);
+	    return solve(n);
 	}
 };
+
+// approach 4 - space optimization
+// not possible here
 
 //{ Driver Code Starts.
 int main(){
