5e06642f9094ea110d42f3aa3c8ad02c60634a97
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-06-14 17:26:31 +0530

    Time: 12 ms (83.56%), Space: 8.5 MB (86.71%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,26 @@
+// https://www.youtube.com/watch?v=slqQfhL6kQU&ab_channel=CodewithAlisha
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+         if(s.size()<p.size())return {};
+        vector<int>phash(26,0);
+        vector<int>shash(26,0);
+        vector<int>ans;
+        for(int i=0;i<p.size();i++)
+        {
+            phash[p[i]-'a']++;
+            shash[s[i]-'a']++;
+        }
+       
+        if(phash==shash)ans.push_back(0);
+        
+        for(int i=p.size();i<s.size();i++)
+        {
+             shash[s[i]-'a']++;
+             shash[s[i-p.size()]-'a']--;
+             if(phash==shash)ans.push_back(i+1-p.size());
+        }
+        
+        return ans;
+    }
+};
