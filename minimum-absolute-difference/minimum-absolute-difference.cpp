63fb1ede448438f735d60e51cfa40a459ba2349e
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-06-16 22:24:24 +0530

    Time: 86 ms, Memory: 32 MB - LeetHub

---
+++ minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());  // Sort the array in ascending order
+        int minDiff = INT_MAX;
+        
+        // Find the minimum absolute difference
+        for (int i = 1; i < arr.size(); i++) {
+            minDiff = min(minDiff, abs(arr[i] - arr[i-1]));
+        }
+        
+        // Find pairs with the minimum absolute difference
+        for (int i = 1; i < arr.size(); i++) {
+            if (abs(arr[i] - arr[i-1]) == minDiff) {
+                ans.push_back({arr[i-1], arr[i]});
+            }
+        }
+        
+        return ans;
+    }
+};
