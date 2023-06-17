6f3d0910a0c1581255861f7352aae01a07857885
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-06-18 00:13:05 +0530

    Time: 190 ms (62.66%), Space: 26 MB (48.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,33 @@
+// For reference -https://www.youtube.com/watch?v=DhFh8Kw7ymk&t=826s&ab_channel=takeUforward
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>>ans;
-    sort(nums.begin() , nums.end());
-    for(int i= 0 ; i<nums.size()-2; i++){
-         
-        if(i == 0 || (i>0 && nums[i]!= nums[i-1])){
-            
-  //kyuki agar i = 0 ha to hum i-1 nhi lga skte h na boss isleye hmne kya kai hmne condition dal di ha agar i = 0 ha to sidhe chle jao nhi h to check krlo kya i or i-1 eleent same to nhi h kyuki agar vo same h to koi faida nhi h same ke lia bar bar check krne se 
-            
-            int low = i+1;
-            int high = nums.size()-1;
-            int sum = 0 - nums[i];
-            while(low < high){
-                 
-                if(nums[low] + nums[high] == sum){
-                      vector<int>x;
-                    x.push_back(nums[i]);
-                    x.push_back(nums[low]);
-                    x.push_back(nums[high]);
-                    ans.push_back(x);
-                    
-                    while(low<high && nums[low] == nums[low+1])
-                        low++; 
-                    while(low<high && nums[high] == nums[high-1])
-                        high--;
-                        
-                    low++; high--;
+        int sum=0;
+        vector<vector<int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
                 }
-                
-                else if (nums[low] + nums[high] < sum){
-                       low++;
+                else if(sum>0){
+                    k--;
                 }
-                else
-                    high--;
+                else{
+                    vector<int> temp = { nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] ==nums[k+1]) k--;
+                }
             }
-            
         }
+        return ans;
     }
-    return ans;
-}
 };
