a300bf9b4b72a77eef58b73b3349dbbaa417f9a2
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-07-18 11:54:05 +0530

    Time: 12 ms (45.84%), Space: 5.9 MB (54.83%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else if(n==2){
+        return 1;
+        }
+        else if(n>2){
+            return fib(n-2)+fib(n-1);
+        }
+        return 1;
+    }
+};
