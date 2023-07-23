ef2263e082bdf99186c5d39cb63dea812ac39054
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-07-23 14:13:14 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -38,55 +38,30 @@ class Solution
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
-        Node* zeroHead = new Node(-1);
-    Node* zeroTail = zeroHead;
-    Node* oneHead = new Node(-1);
-    Node* oneTail = oneHead;
-    Node* twoHead = new Node(-1);
-    Node* twoTail = twoHead;
-    
-    Node* curr = head;
-    
-    // create separate list 0s, 1s and 2s
-    while(curr != NULL) {
         
-        int value = curr -> data;
-        
-        if(value == 0) {
-            insertAtTail(zeroTail, curr);
+        // Add code here
+        int z=0,o=0,t=0;
+        Node* temp=head;
+        while(temp){
+            if(temp->data==0)z++;
+            else if(temp->data==1)o++;
+            else t++;
+            temp=temp->next;
         }
-        else if(value == 1) {
-            insertAtTail(oneTail, curr);
+        temp=head;
+        while(temp){
+            if(z){
+                temp->data=0;
+                z--;
+            }
+            else if(o){
+                temp->data=1;
+                o--;
+            }
+            else temp->data=2;
+            temp=temp->next;
         }
-        else if(value == 2) {
-            insertAtTail(twoTail, curr);
-        }       
-        curr = curr -> next;
-    }
-    
-    //merge 3 sublist
-    
-    // 1s list not empty
-    if(oneHead -> next != NULL) {
-        zeroTail -> next = oneHead -> next;
-    }
-    else {
-        //1s list -> empty
-        zeroTail -> next = twoHead -> next;
-    }
-    
-    oneTail -> next = twoHead -> next;
-    twoTail -> next = NULL;
-    
-	//setup head 
-    head = zeroHead -> next;
-    
-    //delete dummy nodes
-    delete zeroHead;
-    delete oneHead;
-    delete twoHead;
-    
-    return head;
+        return head;
     }
 };
 
