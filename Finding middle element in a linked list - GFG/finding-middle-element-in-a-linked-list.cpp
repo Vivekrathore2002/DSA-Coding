eb0173576b54140b530d1d3f796a280d43be0ad0
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-07-26 22:20:15 +0530

    Added solution - LeetHub

--- Finding middle element in a linked list - GFG/finding-middle-element-in-a-linked-list.cpp
+++ Finding middle element in a linked list - GFG/finding-middle-element-in-a-linked-list.cpp
@@ -37,22 +37,25 @@ struct Node {
     
 }; */
 class Solution{
-public:
-    /* Should return data of middle node. If linked list is empty, then -1 */
-    int getMiddle(Node *head) {
-        if (head == NULL)
-            return -1;
-
-        Node* slow = head;
-        Node* fast = head;
-
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        return slow->data;
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+{
+    if (head == NULL) {
+        // Handle the case when the list is empty
+        // You can return an error code or throw an exception
+        // depending on your requirements.
     }
+    Node* slow = head;
+    Node* fast = head;
+    
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return slow->data;
+}
 };
 
 
