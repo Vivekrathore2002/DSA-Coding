64fdb6b1b47bca2238cfd9003a6829e783f8cb38
Vivek Rathore <90974587+Vivekrathore2002@users.noreply.github.com>
2023-07-27 23:40:15 +0530

    Added solution - LeetHub

---
+++ Delete Middle of Linked List - GFG/delete-middle-of-linked-list.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteMid(struct Node *head);
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		head = deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+
+// } Driver Code Ends
+
+
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+Node* deleteMid(Node* head)
+{
+    if (head == nullptr || head->next == nullptr) {
+        // List has 0 or 1 node, so there is no middle to delete
+        return nullptr;
+    }
+    Node* prev=NULL;
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    if (prev != nullptr) {
+        prev->next = slow->next;
+    } else {
+        // If 'prev' is nullptr, it means the list had only two nodes, and we are deleting the first one
+        head = slow->next;
+    }
+    return head;
+}
